22ef6dcc62b4e9f5515abcca3ab179b55f512d23
stkudryashov <aleks59niki30@gmail.com>
2021-04-07 20:49:00 +0300

    add admin columns

--- cryptotrading/trading/admin.py
+++ cryptotrading/trading/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from trading.models import ProposalBtc, ExchangePoint
 
 
-admin.site.register(ProposalBtc)
-admin.site.register(ExchangePoint)
+@admin.register(ProposalBtc)
+class ProposalBtcAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_telegram_id', 'buy', 'point_name', 'date')
+    list_display_links = ('id', 'user_telegram_id')
+
+
+@admin.register(ExchangePoint)
+class ExchangePointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'stocks', 'sells')
+    list_display_links = ('id', 'name')
