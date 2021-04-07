b4ea4bff4e2f67d0861a4f643d4e1aa06a05e558
stkudryashov <aleks59niki30@gmail.com>
2021-04-07 22:50:43 +0300

    changed order

--- cryptotrading/trading/management/commands/bot.py
+++ cryptotrading/trading/management/commands/bot.py
@@ -29,7 +29,36 @@ def btc_bot_new_messages(update: Update, context: CallbackContext):
     if ProposalBtc.objects.filter(user_telegram_id=user_telegram_id).count() == 1:
         proposal_btc = ProposalBtc.objects.get(user_telegram_id=user_telegram_id)
 
-        if not proposal_btc.is_number:
+        if not proposal_btc.is_count:
+            try:
+                value = float(message.replace(',', '.'))
+
+                if proposal_btc.buy:
+                    exchange_points = ExchangePoint.objects.filter(sells__gte=value)
+                else:
+                    exchange_points = ExchangePoint.objects.filter(stocks__gte=value)
+
+                if exchange_points:
+                    keyboard = []
+                    for exchange_point in exchange_points:
+                        keyboard.append([InlineKeyboardButton(text=exchange_point.name,
+                                                              callback_data='ep {}'.format(exchange_point.name))])
+
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+                    proposal_btc.count = value
+                    proposal_btc.is_count = True
+                    proposal_btc.save()
+
+                    bot_message = 'Теперь выбери точку обмена'
+                    bot.sendMessage(user_telegram_id, bot_message, reply_markup=keyboard)
+                else:
+                    bot_message = 'У точек нет столько денег :с\nПопробуй ввести сумму меньше'
+                    bot.sendMessage(user_telegram_id, bot_message)
+            except ValueError:
+                bot_message = 'Я тебя не понял, попробуй написать сумму еще раз'
+                bot.sendMessage(user_telegram_id, bot_message)
+        elif not proposal_btc.is_number:
             if len(message) < 11:
                 bot_message = 'Я не понимаю твоего номера'
             else:
@@ -37,61 +66,20 @@ def btc_bot_new_messages(update: Update, context: CallbackContext):
                 proposal_btc.is_number = True
                 proposal_btc.save()
 
-                bot_message = 'Я запомнил твой номер :)\nТеперь напиши точку обмена'
-
-            bot.sendMessage(user_telegram_id, bot_message)
-        elif not proposal_btc.is_point:
-            point_name = ExchangePoint.objects.filter(name=message)
-
-            if point_name:
-                proposal_btc.point_name = message
-                proposal_btc.is_point = True
-                proposal_btc.save()
-
-                bot_message = 'Я нашел эту точку :)\nВыбери что тебя интересует'
-
-                keyboard = InlineKeyboardMarkup(
-                    inline_keyboard=[[InlineKeyboardButton(text='Купить', callback_data='buy'),
-                                      InlineKeyboardButton(text='Продать', callback_data='sell')]])
-
-                bot.sendMessage(chat_id=user_telegram_id, text=bot_message, reply_markup=keyboard)
-            else:
-                bot_message = 'Я не смог найти такую точку :с\nПопробуй написать название точки еще раз'
-                bot.sendMessage(user_telegram_id, bot_message)
-
-        elif not proposal_btc.is_count:
-            try:
-                value = float(message.replace(',', '.'))
-                exchange_point = ExchangePoint.objects.get(name=proposal_btc.point_name)
-
-                if proposal_btc.buy:
-                    exchange_point_value = exchange_point.sells
-                else:
-                    exchange_point_value = exchange_point.stocks
-
-                if exchange_point_value < value:
-                    bot_message = 'У точки нет столько денег :с'
-                else:
-                    proposal_btc.count = float(message.replace(',', '.'))
-                    proposal_btc.is_count = True
-                    proposal_btc.save()
-
-                    bot_message = 'Все отлично, ваша заявка создана'
-            except ValueError:
-                bot_message = 'Я тебя не понял, попробуй написать сумму еще раз'
+                bot_message = 'Ура! Твоя заявка готова'
 
             bot.sendMessage(user_telegram_id, bot_message)
         else:
-            pass
+            bot.sendMessage(user_telegram_id, 'Заявка уже создана')
     else:
         if message == '/start' or message == 'начать':
-            bot_message = 'Привет, я бот по обмену крипты :)\nДля начала напиши номер телефона'
+            bot_message = 'Привет, я бот по обмену крипты :)\nДля что ты хочешь сделать?'
 
-            ProposalBtc.objects.create(
-                user_telegram_id=user_telegram_id,
-            )
+            keyboard = InlineKeyboardMarkup(
+                inline_keyboard=[[InlineKeyboardButton(text='Купить', callback_data='buy'),
+                                  InlineKeyboardButton(text='Продать', callback_data='sell')]])
 
-            bot.sendMessage(chat_id=user_telegram_id, text=bot_message)
+            bot.sendMessage(chat_id=user_telegram_id, text=bot_message, reply_markup=keyboard)
         else:
             bot_message = 'Я тебя не понял, воспользуйся кнопкой ниже, чтобы начать обмен'
 
@@ -135,27 +123,28 @@ def btc_bot_edit_messages(update: Update, context: CallbackContext):
         except telepot.exception.TelegramError:
             pass
         finally:
-            bot_message = 'Привет, я бот по обмену крипты :)\nДля начала напиши номер телефона'
+            bot_message = 'Привет, я бот по обмену крипты :)\nДля что ты хочешь сделать?'
 
-            ProposalBtc.objects.create(
-                user_telegram_id=user_telegram_id,
-            )
+            keyboard = InlineKeyboardMarkup(
+                inline_keyboard=[[InlineKeyboardButton(text='Купить', callback_data='buy'),
+                                  InlineKeyboardButton(text='Продать', callback_data='sell')]])
 
-            bot.sendMessage(chat_id=user_telegram_id, text=bot_message)
+            bot.sendMessage(chat_id=user_telegram_id, text=bot_message, reply_markup=keyboard)
     elif 'buy' in button_press:
         try:
             bot.deleteMessage(edit_message)
         except telepot.exception.TelegramError:
             pass
         finally:
-            proposal_btc = ProposalBtc.objects.get(user_telegram_id=user_telegram_id)
-            percent = got_now_btc_run() * ExchangePoint.objects.get(name=proposal_btc.point_name).percent_sell
+            percent = got_now_btc_run() * 0.015
 
             bot_text = "Текущий курс: \n\n 1 BTC -> {0} RUB\n\n".format(round(got_now_btc_run() - percent, 2))
             bot_text += "На какую сумму ₽ хотите купить BTC?"
 
-            proposal_btc.buy = True
-            proposal_btc.save()
+            ProposalBtc.objects.create(
+                user_telegram_id=user_telegram_id,
+                buy=True
+            )
 
             bot.sendMessage(chat_id=user_telegram_id, text=bot_text)
     elif 'sell' in button_press:
@@ -164,13 +153,30 @@ def btc_bot_edit_messages(update: Update, context: CallbackContext):
         except telepot.exception.TelegramError:
             pass
         finally:
-            proposal_btc = ProposalBtc.objects.get(user_telegram_id=user_telegram_id)
-            percent = got_now_btc_run() * ExchangePoint.objects.get(name=proposal_btc.point_name).percent_buy
+            percent = got_now_btc_run() * 0.02
 
             bot_text = "Текущий курс: \n\n 1 BTC -> {0} RUB\n\n".format(round(got_now_btc_run() + percent, 2))
             bot_text += "На какую сумму ₽ хотите продать BTC?"
 
+            ProposalBtc.objects.create(
+                user_telegram_id=user_telegram_id
+            )
+
             bot.sendMessage(chat_id=user_telegram_id, text=bot_text)
+    elif 'ep' in button_press:
+        try:
+            bot.deleteMessage(edit_message)
+        except telepot.exception.TelegramError:
+            pass
+        finally:
+            proposal_btc = ProposalBtc.objects.get(user_telegram_id=user_telegram_id)
+            proposal_btc.point_name = button_press.split(' ')[1]
+            proposal_btc.is_point = True
+            proposal_btc.save()
+
+            bot_message = 'Отлично! Теперь введи свой номер телефона'
+
+            bot.sendMessage(chat_id=user_telegram_id, text=bot_message)
 
 
 class Command(BaseCommand):
--- cryptotrading/trading/models.py
+++ cryptotrading/trading/models.py
@@ -9,8 +9,9 @@ class ProposalBtc(models.Model):
     is_count = models.BooleanField(default=False)  # Написал ли сумму
     count = models.FloatField(blank=True, null=True)  # Сумма
     is_point = models.BooleanField(default=False)  # Написал ли точку обмена
-    point_name = models.CharField(max_length=16, verbose_name='exchange point', blank=True, null=True)  # точка обмена
+    point_name = models.CharField(max_length=16, verbose_name='exchange point', blank=True, null=True)  # Точка обмена
     date = models.DateTimeField(auto_now_add=True)  # Дата заявки
+    date_visit = models.CharField(max_length=16, verbose_name='date of visit', blank=True, null=True)  # Дата визита
 
     def __str__(self):
         return '{}-{}'.format(self.user_telegram_id, self.point_name)
@@ -24,8 +25,6 @@ class ExchangePoint(models.Model):
     name = models.CharField(max_length=16, verbose_name='exchange point')
     stocks = models.FloatField(default=0, verbose_name='how much is in stock')
     sells = models.FloatField(default=0, verbose_name='how much can sell')
-    percent_buy = models.FloatField(default=0, verbose_name='percent buy')
-    percent_sell = models.FloatField(default=0, verbose_name='percent sell')
 
     def __str__(self):
         return self.name
